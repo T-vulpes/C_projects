716325dc8214eb201f20f9d2d76ac88ce692fe4c
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-08-14 15:48:09 +0300

    main.c

--- sincos/main.c
+++ sincos/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>  // Include the math library for sin and cos functions
+#include <math.h>  
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
     double degree, resultSin, resultCos;
