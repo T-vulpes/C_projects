05c695130ec7682726b6ee736aa683f2179d713a
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-08-14 15:49:35 +0300

    mn.c

--- tictactoe_game/mn.c
+++ tictactoe_game/mn.c
@@ -32,7 +32,6 @@ int checkWin(char board[3][3]) {
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
             return 1;
     }
-    // Check diagonals
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
         return 1;
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
