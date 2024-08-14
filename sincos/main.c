8c92694960af9414f808385067bc6ad66605f3ed
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-08-14 15:48:18 +0300

    main.c

--- sincos/main.c
+++ sincos/main.c
@@ -7,9 +7,9 @@ int main(int argc, char *argv[]) {
     double degree, resultSin, resultCos;
     printf("Enter the degree: ");
     scanf("%lf", &degree);
-    resultSin = sin(degree * M_PI / 180.0);  // Convert degree to radians
+    resultSin = sin(degree * M_PI / 180.0);  
     printf("The sine value of the degree: %lf\n", resultSin);
-    resultCos = cos(degree * M_PI / 180.0);  // Convert degree to radians
+    resultCos = cos(degree * M_PI / 180.0);  
     printf("The cosine value of the degree: %lf", resultCos);
     return 0;
 }
