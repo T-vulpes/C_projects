7d513aaa5017db8a2d0834b1742406c63df63973
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-04-28 13:34:46 +0300

    main.c

--- armstorng/main.c
+++ armstorng/main.c
@@ -23,13 +23,11 @@ int main() {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-
     if (isArmstrong(num)) {
         printf("%d is an Armstrong number.\n", num);
     } else {
         printf("%d is not an Armstrong number.\n", num);
     }
-
     return 0;
 }
 
